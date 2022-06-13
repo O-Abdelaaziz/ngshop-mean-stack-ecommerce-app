bf4ffb2aee33fae9998ab1798141fe0d796171df
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-06-13 14:21:27 +0100

    Task 6: create first api call   parsing json data

--- server/app.js
+++ server/app.js
@@ -4,9 +4,21 @@ const app = express();
 require("dotenv/config");
 const api = process.env.API_URL;
 
-app.get("/", (req, res) => {
-  console.log(api);
-  res.send("Hello API!!!");
+//Middleware
+app.use(express.json());
+
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "hair dresser",
+    image: "some_url",
+  };
+  res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const product = req.body;
+  res.send(product);
 });
 
 app.listen(3000, () => {});
