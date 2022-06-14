b1bf220270d2695cd9d0e0408e5cb49c8b12009d
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-06-14 09:37:10 +0100

    Task 13: working with product model & schema

--- server/models/product.js
+++ server/models/product.js
@@ -1,13 +1,62 @@
 const mongoose = require("mongoose");
+const { required } = require("nodemon/lib/config");
+const { Category } = require("./category");
 
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-      type: Number,
-      required: true,
+  id: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  shortDescription: {
+    type: String,
+    required: true,
+  },
+  longDescription: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [
+    {
+      type: String,
     },
-  });
+  ],
+  brand: {
+    type: String,
+    default: "",
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: "",
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    required: "",
+    min: 1,
+    max: 5,
+  },
+  isFeatured: {
+    type: Date,
+    default: false,
+  },
+  createdDate: {
+    type: Date,
+  },
+});
 
-  
 exports.Product = mongoose.model("Product", productSchema);
