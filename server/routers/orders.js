ed181d91e5d57d7caa4bc1934e19d61634e83aa7
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-06-17 21:16:49 +0100

    Task 42: update order status & delete order

--- server/routers/orders.js
+++ server/routers/orders.js
@@ -68,4 +68,36 @@ router.post(`/`, async (req, res) => {
   res.send(order);
 });
 
+router.put("/:id", async (req, res) => {
+  const order = await Order.findByIdAndUpdate(
+    req.params.id,
+    {
+      status: req.body.status,
+    },
+    { new: true }
+  );
+
+  if (!order) return res.status(400).send("the order cannot be update!");
+
+  res.send(order);
+});
+
+router.delete("/:id", (req, res) => {
+  Order.findByIdAndRemove(req.params.id)
+    .then(async (order) => {
+      if (order) {
+        return res
+          .status(200)
+          .json({ success: true, message: "the order is deleted!" });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: "order not found!" });
+      }
+    })
+    .catch((err) => {
+      return res.status(500).json({ success: false, error: err });
+    });
+});
+
 module.exports = router;
