9031e7d84c02fbc5710f8559bbbda64e18fa9204
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-06-17 21:42:40 +0100

    Task 44: calculating total price of one order

--- server/routers/orders.js
+++ server/routers/orders.js
@@ -47,6 +47,19 @@ router.post(`/`, async (req, res) => {
 
   const orderItemsIdsResolved = await orderItemsIds;
 
+  const totalPrices = await Promise.all(
+    orderItemsIdsResolved.map(async (orderItemId) => {
+      const orderItem = await OrderItem.findById(orderItemId).populate(
+        "product",
+        "price"
+      );
+      const totalPrice = orderItem.product.price * orderItem.quantity;
+      return totalPrice;
+    })
+  );
+
+  const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+
   let order = new Order({
     orderItems: orderItemsIdsResolved,
     shippingAddress1: req.body.shippingAddress1,
