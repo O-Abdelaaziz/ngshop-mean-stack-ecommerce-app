a88a0d56dea81c31b94d0a8de883fb82848e3e99
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-06-17 12:03:53 +0100

    Task 29: hashing the user password

--- server/routers/users.js
+++ server/routers/users.js
@@ -1,6 +1,7 @@
-const {User} = require("../models/user");
+const { User } = require("../models/user");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 router.get(`/`, async (req, res) => {
   const userList = await User.find();
@@ -13,26 +14,24 @@ router.get(`/`, async (req, res) => {
   res.send(userList);
 });
 
-
-router.post('/', async (req,res)=>{
+router.post("/", async (req, res) => {
   let user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      passwordHash: req.body.passwordHash,
-      phone: req.body.phone,
-      isAdmin: req.body.isAdmin,
-      street: req.body.street,
-      apartment: req.body.apartment,
-      zip: req.body.zip,
-      city: req.body.city,
-      country: req.body.country,
-  })
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
   user = await user.save();
 
-  if(!user)
-  return res.status(400).send('the user cannot be created!')
+  if (!user) return res.status(400).send("the user cannot be created!");
 
   res.send(user);
-})
+});
 
 module.exports = router;
