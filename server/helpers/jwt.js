d96f89909e6ccadff8db45d169d97562376c1419
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-06-17 15:53:47 +0100

    Task 35: excluding rest api route from authentication

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -9,9 +9,17 @@ const { expressjwt: expressJwt } = require("express-jwt");
 
 function authenticationJwt() {
   const secret = process.env.SECRET;
+  const api = process.env.API_URL;
   return expressJwt({
     secret,
     algorithms: ["HS256"],
+  }).unless({
+    path:[
+      {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
+      {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+      `${api}/users/login`,
+      `${api}/users/register`,
+  ]
   });
 }
 
