22f189c5db610045eb31047a2a5d4f9f98263b1e
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-06-17 14:55:04 +0100

    Task 32: login a user rest api & creating a token

--- server/routers/users.js
+++ server/routers/users.js
@@ -2,6 +2,7 @@ const { User } = require("../models/user");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.get(`/`, async (req, res) => {
   const userList = await User.find().select("-passwordHash");
@@ -43,38 +44,58 @@ router.post("/", async (req, res) => {
   res.send(user);
 });
 
-router.put('/:id',async (req, res)=> {
-
+router.put("/:id", async (req, res) => {
   const userExist = await User.findById(req.params.id);
-  let newPassword
-  if(req.body.password) {
-      newPassword = bcrypt.hashSync(req.body.password, 10)
+  let newPassword;
+  if (req.body.password) {
+    newPassword = bcrypt.hashSync(req.body.password, 10);
   } else {
-      newPassword = userExist.passwordHash;
+    newPassword = userExist.passwordHash;
   }
 
   const user = await User.findByIdAndUpdate(
-      req.params.id,
-      {
-          name: req.body.name,
-          email: req.body.email,
-          passwordHash: newPassword,
-          phone: req.body.phone,
-          isAdmin: req.body.isAdmin,
-          street: req.body.street,
-          apartment: req.body.apartment,
-          zip: req.body.zip,
-          city: req.body.city,
-          country: req.body.country,
-      },
-      { new: true}
-  )
+    req.params.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      passwordHash: newPassword,
+      phone: req.body.phone,
+      isAdmin: req.body.isAdmin,
+      street: req.body.street,
+      apartment: req.body.apartment,
+      zip: req.body.zip,
+      city: req.body.city,
+      country: req.body.country,
+    },
+    { new: true }
+  );
 
-  if(!user)
-  return res.status(400).send('the user cannot be created!')
+  if (!user) return res.status(400).send("the user cannot be created!");
 
   res.send(user);
-})
+});
 
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  const secret = process.env.SECRET;
+  if (!user) {
+    return res.status(400).send("The user not found");
+  }
+
+  if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        isAdmin: user.isAdmin,
+      },
+      secret,
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).send({ user: user.email, token: token });
+  } else {
+    res.status(400).send("password is wrong!");
+  }
+});
 
 module.exports = router;
