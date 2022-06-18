bfabb9262386486eb25308b1659f30f1ae408630
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-06-18 11:22:50 +0100

    Task 50: working with validating uploaded file types

--- server/routers/products.js
+++ server/routers/products.js
@@ -5,13 +5,27 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const multer = require("multer");
 
+const FILE_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/uploads");
+    const isValid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error("invalid image type");
+
+    if (isValid) {
+      uploadError = null;
+    }
+    cb(uploadError, "public/uploads");
   },
+
   filename: function (req, file, cb) {
     const fileName = file.originalname.split(" ").join("-");
-    cb(null, fileName + "-" + Date.now());
+    const extension = FILE_TYPE_MAP[file.mimetype];
+    cb(null, `${fileName}-${Date.now()}.${extension}`);
   },
 });
 
