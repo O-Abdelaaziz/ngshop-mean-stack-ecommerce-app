9e5af994a28c12bc68800ede207525a767a569b3
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-06-20 08:29:38 +0100

    Task 57: creating application level components (home-page, product-list)

--- client/ouakala-workspace/apps/ngshop/src/app/app.module.ts
+++ client/ouakala-workspace/apps/ngshop/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product/product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent],
+    imports: [BrowserModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
