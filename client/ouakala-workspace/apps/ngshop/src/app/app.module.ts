e747e94da3344c9ed7ee591a44be91c7df70d8af
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-06-20 13:53:27 +0100

    Task 64: creating component inside librarie and use them in the apps (banner, slider)

--- client/ouakala-workspace/apps/ngshop/src/app/app.module.ts
+++ client/ouakala-workspace/apps/ngshop/src/app/app.module.ts
@@ -8,10 +8,10 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product/product-list/product-list.component';
 import { PrimaryHeaderComponent } from './shared/primary-header/primary-header.component';
 import { PrimaryFooterComponent } from './shared/primary-footer/primary-footer.component';
-
+import { UiModule } from '@ouakala-workspace/ui';
 @NgModule({
     declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent, PrimaryHeaderComponent, PrimaryFooterComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, UiModule],
     providers: [],
     bootstrap: [AppComponent]
 })
