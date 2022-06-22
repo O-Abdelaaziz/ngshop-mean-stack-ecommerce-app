a9246afde9a5e4e6efcecd8359b1c0101ce31bb2
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-06-22 08:55:43 +0100

    Task 75: categorie service & get data from backend

---
+++ client/ouakala-workspace/libs/products/src/lib/services/category.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CategoryService {
+    public baseUrl = 'http://localhost:3000/api/v1/categories';
+
+    constructor(private _httpClient: HttpClient) {}
+
+    public getCategories(): Observable<Category[]> {
+        return this._httpClient.get<Category[]>(this.baseUrl);
+    }
+}
