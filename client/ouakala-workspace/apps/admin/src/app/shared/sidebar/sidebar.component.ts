ebafbf3819d0cbfef9cc698a1dde50f932bfc4e2
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-06-25 11:22:59 +0100

    Task 114: logout user from admin panel & remove token from local storage

--- client/ouakala-workspace/apps/admin/src/app/shared/sidebar/sidebar.component.ts
+++ client/ouakala-workspace/apps/admin/src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '@ouakala-workspace/users';
 
 @Component({
-  selector: 'admin-sidebar',
-  templateUrl: './sidebar.component.html',
-  // styleUrls: ['./sidebar.component.scss']
+    selector: 'admin-sidebar',
+    templateUrl: './sidebar.component.html'
+    // styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+    constructor(private _authenticationService: AuthenticationService) {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+    public onLogout() {
+        this._authenticationService.logout();
+    }
 }
--- client/ouakala-workspace/libs/users/src/lib/services/authentication.service.ts
+++ client/ouakala-workspace/libs/users/src/lib/services/authentication.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
     providedIn: 'root'
@@ -9,9 +11,13 @@ import { User } from '../models/user';
 export class AuthenticationService {
     public baseUrl = 'http://localhost:3000/api/v1/users';
 
-    constructor(private _httpClient: HttpClient) {}
+    constructor(private _httpClient: HttpClient, private _localStorageService: LocalStorageService, private _router: Router) {}
 
     public login(email: string, password: string): Observable<User> {
         return this._httpClient.post<User>(`${this.baseUrl}/login`, { email, password });
+    }
+    public logout() {
+        this._localStorageService.removeToken();
+        this._router.navigate(['/login']);
     }
 }
