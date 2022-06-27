bb8624c6f48c136fc9cf256b7255b722efa7953d
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-06-27 08:51:11 +0100

    Task 121: working with styling banner component & removing library in nx

--- client/ouakala-workspace/libs/ui/src/lib/ui.module.ts
+++ client/ouakala-workspace/libs/ui/src/lib/ui.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './components/banner/banner.component';
 import { SliderComponent } from './components/slider/slider.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-    imports: [CommonModule],
-    declarations: [
-      BannerComponent,
-      SliderComponent
-    ],
-    exports: [
-      BannerComponent,
-      SliderComponent
-    ]
+    declarations: [BannerComponent, SliderComponent],
+    imports: [CommonModule, ButtonModule],
+    exports: [BannerComponent, SliderComponent, ButtonModule]
 })
 export class UiModule {}
