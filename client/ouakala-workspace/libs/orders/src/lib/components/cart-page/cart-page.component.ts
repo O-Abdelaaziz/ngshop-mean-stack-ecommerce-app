d732f32e6b3a2e90dac88763e13d2980c58e0967
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-01 13:35:02 +0100

    Task 141: connect the cart with products

--- client/ouakala-workspace/libs/orders/src/lib/components/cart-page/cart-page.component.ts
+++ client/ouakala-workspace/libs/orders/src/lib/components/cart-page/cart-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { ProductService } from '@ouakala-workspace/products';
 
 @Component({
     selector: 'orders-cart-page',
@@ -7,16 +9,30 @@ import { Router } from '@angular/router';
     styles: []
 })
 export class CartPageComponent implements OnInit {
-    constructor(private _router: Router) {}
+    constructor(private _router: Router, private _cartService: CartService, private _productService: ProductService) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.getCartDetails();
+    }
 
     public OnBackToShop() {
         this._router.navigate(['/products']);
     }
 
-    public onRemoveCartItem(){
-      console.log("clicked");
+    public onRemoveCartItem() {
+        console.log('clicked');
+    }
 
+    private getCartDetails() {
+        this._cartService.cart$.pipe().subscribe((response) => {
+            response.items?.forEach((cartItem) => {
+                this._productService.getProductById(cartItem.productId as string).subscribe(
+                  (response)=>{
+                    console.log(response);
+
+                  }
+                )
+            });
+        });
     }
 }
