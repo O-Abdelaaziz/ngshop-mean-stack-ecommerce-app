37531f1d2f7e52f486052706f75e406d38434dd7
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-03 11:56:24 +0100

    Task 150: create ngrx state store in user library

--- client/ouakala-workspace/libs/users/src/lib/users.module.ts
+++ client/ouakala-workspace/libs/users/src/lib/users.module.ts
@@ -6,11 +6,26 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromUsers from './+state/users.reducer';
+import { UsersEffects } from './+state/users.effects';
+import { UsersFacade } from './+state/users.facade';
 // import { RouterModule, Route } from '@angular/router';
 // export const usersRoutes: Route[] = [];
 
 @NgModule({
-    imports: [CommonModule, UsersRoutingModule,FormsModule, ReactiveFormsModule, ButtonModule, InputTextModule],
+    imports: [
+        CommonModule,
+        UsersRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ButtonModule,
+        InputTextModule,
+        StoreModule.forFeature(fromUsers.USERS_FEATURE_KEY, fromUsers.reducer),
+        EffectsModule.forFeature([UsersEffects])
+    ],
     declarations: [LoginComponent],
+    providers: [UsersFacade]
 })
 export class UsersModule {}
